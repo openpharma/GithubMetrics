7885917e244ff12caafc78f7dc36ed232f74005f
James Black <james@epijim.uk>
2021-01-28 22:28:35 +0100

    add a test

--- tests/testthat/test-gh_repos_XXX.R
+++ tests/testthat/test-gh_repos_XXX.R
@@ -6,9 +6,28 @@ organisation <- "openpharma"
 
 test_that("Can connect and see repos in org", {
   vcr::use_cassette("gh_repos_get", {
-    x <- length(gh_repos_get(org = organisation)) > 3
+    x <- gh_repos_get(org = organisation)
   })
-  expect_true(x)
+
+  x <- length(x) > 3
+
+  expect_equal(x)
 })
+
+test_that("Clean the repos", {
+  vcr::use_cassette("gh_repos_get", {
+    x <- gh_repos_get(org = organisation)
+  })
+
+  x <- gh_repos_clean(x)
+
+  expect_equal(
+    names(x),
+    c("name","full_name","size","updated_at","default_branch","language","MB")
+  )
+
+  expect_true(nrow(x) > 0)
+})
+
 
 
