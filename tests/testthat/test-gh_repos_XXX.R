cfd912a0660ca1cdb031586e633750d9e5afc7e5
James Black <james@epijim.uk>
2021-01-29 22:42:57 +0100

    fix issue with tests

--- tests/testthat/test-gh_repos_XXX.R
+++ tests/testthat/test-gh_repos_XXX.R
@@ -4,27 +4,32 @@ library(testthat)
 
 organisation <- "openpharma"
 
-test_that("Can connect and see repos in org", {
-  vcr::use_cassette("gh_repos_get", {
-    x <- gh_repos_get(org = organisation)
-  })
-
-  x <- length(x) > 3
-
-  expect_equal(x)
+vcr::use_cassette("gh_repos_get", {
+  gh_repos_get <- gh_repos_get(org = organisation)
 })
 
-test_that("Clean the repos", {
-  vcr::use_cassette("gh_repos_get", {
-    x <- gh_repos_get(org = organisation)
-  })
+test_that("Can connect and see repos in org", {
 
-  x <- gh_repos_clean(x)
+
+  x <- length(gh_repos_get) > 3
+
+  expect_true(x)
+})
+
+test_that("Clean the repos has write vars", {
+
+  x <- gh_repos_clean(gh_repos_get)
 
   expect_equal(
     names(x),
     c("name","full_name","size","updated_at","default_branch","language","MB")
   )
+
+})
+
+test_that("Clean the repos has some data", {
+
+  x <- gh_repos_clean(gh_repos_get)
 
   expect_true(nrow(x) > 0)
 })
