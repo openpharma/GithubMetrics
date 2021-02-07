7e3461f95fcf353a585eaf65e7dc156458bfc8ee
James Black <james@epijim.uk>
2021-02-07 17:22:36 +0100

    first draft

---
+++ R/gh_user_events_get.R
@@ -0,0 +1,36 @@
+#' Get a users activity in last 90 days (if authenticated also get private for yourself)
+#'
+#' @param user The github username (if authenticated will see private as well).
+#' @param ... Pass down to \code{gh}
+#'
+#' @importFrom magrittr "%>%"
+#'
+#' @export
+#'
+#'
+gh_user_event_get <- function(
+  user,
+  ...
+) {
+
+  events <- gh::gh("/users/:user/events",
+                    user = user,
+                    .limit = Inf,
+                    .progress = FALSE,
+                    ...
+  )
+
+  tibble::tibble(
+    type = events %>% purrr::map_chr("type"),
+    date = events %>% purrr::map_chr("created_at") %>% as.Date(),
+    repo = events %>% purrr::map("repo") %>% purrr::map_chr("name")
+  )
+
+}
+
+
+
+
+
+
+
